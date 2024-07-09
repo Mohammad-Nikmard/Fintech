eb359c0271982596626056f050edeac1fba4c4f3
MohammadNikmard <mnikmard2004@gmail.com>
2024-07-09 20:29:42 +0330

    feat: add color pallete in app

---
+++ lib/constatns/color_constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const blackColor = Color(0xff23303B);
+  static const greyColor600 = Color(0xff8E949A);
+  static const greyColor400 = Color(0xffA4A9AE);
+  static const greyColor200 = Color(0xffA4A9AE);
+  static const redDark = Color(0xffFF6363);
+  static Color redLight = redDark.withOpacity(0.15);
+  static const greenDark = Color(0xff13C999);
+  static Color greenLight = greenDark.withOpacity(0.15);
+  static const blueColor = Color(0xff456EFE);
+  static Color blueThin = blueColor.withOpacity(0.3);
+  static Color blueLight = blueColor.withOpacity(0.15);
+}
