dbf8fb8eaf28600ff91cde691010ffd6df0dac8c
MohammadNikmard <mnikmard2004@gmail.com>
2024-07-25 18:45:29 +0330

    feat: add transfer confirmation section

--- lib/ui/bank_transfer_screen.dart
+++ lib/ui/bank_transfer_screen.dart
@@ -69,6 +69,18 @@ class _BankTransferScreenState extends State<BankTransferScreen> {
                         },
                       ),
                     ),
+                  } else if (progressIndicator == 3) ...{
+                    Padding(
+                      padding: const EdgeInsets.only(top: 80),
+                      child: _TransferConfirmationSection(
+                        mediaQuery: widget.mediaQuery,
+                        onTapped: () {
+                          setState(() {
+                            progressIndicator = 4;
+                          });
+                        },
+                      ),
+                    )
                   }
                 ],
               ),
@@ -838,6 +850,241 @@ class _TransferDescriptionSection extends StatelessWidget {
             ),
           ),
         ),
+      ],
+    );
+  }
+}
+
+class _TransferConfirmationSection extends StatelessWidget {
+  const _TransferConfirmationSection({
+    required this.mediaQuery,
+    required this.onTapped,
+  });
+  final MediaQueryHandler mediaQuery;
+  final VoidCallback onTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Center(
+          child: Text(
+            'Are you sure?',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'SSB',
+              fontSize: 32,
+              color: AppColor.blueColor,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Center(
+          child: Text(
+            'We care about your privacy, please make sure that you want to transfer money.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'SM',
+              fontSize: 18,
+              color: AppColor.greyColor200,
+            ),
+          ),
+        ),
+        const SizedBox(height: 70),
+        Stack(
+          clipBehavior: Clip.none,
+          alignment: AlignmentDirectional.topCenter,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20),
+              ),
+              child: SizedBox(
+                height: 369,
+                width: mediaQuery.screenWidth(context),
+                child: ColoredBox(
+                  color: context.secondaryContainer,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 35, right: 35, top: 56),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Mohammad Nikmard',
+                          style: TextStyle(
+                            fontFamily: 'SSB',
+                            fontSize: 26,
+                            color: AppColor.blackColor,
+                          ),
+                        ),
+                        const Text(
+                          '*******4183',
+                          style: TextStyle(
+                            fontFamily: 'SM',
+                            fontSize: 16,
+                            color: AppColor.greyColor200,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          child: SizedBox(
+                            height: 38,
+                            width: 249,
+                            child: ColoredBox(
+                              color: AppColor.redLight,
+                              child: const Center(
+                                child: Text(
+                                  'Transaction Status: Pending',
+                                  style: TextStyle(
+                                    fontFamily: 'SSB',
+                                    fontSize: 16,
+                                    color: AppColor.redDark,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        RichText(
+                          text: const TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '\$',
+                                style: TextStyle(
+                                  fontFamily: 'SR',
+                                  fontSize: 36,
+                                  color: AppColor.blackColor,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '250.00',
+                                style: TextStyle(
+                                  fontFamily: 'SB',
+                                  fontSize: 36,
+                                  color: AppColor.blackColor,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'USD',
+                                style: TextStyle(
+                                  fontFamily: 'SSB',
+                                  fontSize: 18,
+                                  color: AppColor.greyColor200,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Column(
+                          children: [
+                            const Row(
+                              children: [
+                                Text(
+                                  'Card Type',
+                                  style: TextStyle(
+                                    fontFamily: 'SM',
+                                    fontSize: 18,
+                                    color: AppColor.greyColor200,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  'Debit Card',
+                                  style: TextStyle(
+                                    fontFamily: 'SM',
+                                    fontSize: 18,
+                                    color: AppColor.blackColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 15),
+                            const Divider(
+                              height: 0,
+                              thickness: 1.5,
+                              color: Color(0xffe5e7e9),
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              children: [
+                                const Text(
+                                  'Transfer Fee',
+                                  style: TextStyle(
+                                    fontFamily: 'SM',
+                                    fontSize: 18,
+                                    color: AppColor.greyColor200,
+                                  ),
+                                ),
+                                const Spacer(),
+                                RichText(
+                                  text: const TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: '\$',
+                                        style: TextStyle(
+                                          fontFamily: 'SR',
+                                          fontSize: 18,
+                                          color: AppColor.blackColor,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: '0.00',
+                                        style: TextStyle(
+                                          fontFamily: 'SB',
+                                          fontSize: 18,
+                                          color: AppColor.blackColor,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text: 'USD',
+                                        style: TextStyle(
+                                          fontFamily: 'SM',
+                                          fontSize: 10,
+                                          color: AppColor.blackColor,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              top: -45,
+              child: CircleAvatar(
+                radius: 45,
+                backgroundImage: AssetImage('assets/images/my_photo.jpg'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 58),
+        SizedBox(
+            height: 63,
+            width: mediaQuery.screenWidth(context),
+            child: ElevatedButton(
+              onPressed: () {
+                onTapped();
+              },
+              child: const Text(
+                'Send',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )),
       ],
     );
   }
