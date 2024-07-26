462027d4d276dd3e673d5dc84ca7c8cdcf05b3a4
MohammadNikmard <mnikmard2004@gmail.com>
2024-07-26 12:35:39 +0330

    feat: add netflix payment fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:fintech/DI/service_locator.dart';
 import 'package:fintech/theme/light_theme.dart';
-import 'package:fintech/ui/bank_transfer_screen.dart';
+import 'package:fintech/ui/netflix_payment_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BankTransferScreen(
+      home: NetflixPaymentScreen(
         mediaQuery: locator.get(),
       ),
       theme: lightTheme,
---
+++ lib/ui/netflix_payment_screen.dart
@@ -0,0 +1,381 @@
+import 'package:fintech/constatns/color_constants.dart';
+import 'package:fintech/util/extensions/string_extension.dart';
+import 'package:fintech/util/extensions/theme_extension.dart';
+import 'package:fintech/util/mediaquery_handler.dart';
+import 'package:fintech/widget/custom_appbar.dart';
+import 'package:fintech/widget/custom_box.dart';
+import 'package:fintech/widget/my_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class NetflixPaymentScreen extends StatefulWidget {
+  const NetflixPaymentScreen({
+    super.key,
+    required this.mediaQuery,
+  });
+  final MediaQueryHandler mediaQuery;
+
+  @override
+  State<NetflixPaymentScreen> createState() => _NetflixPaymentScreenState();
+}
+
+class _NetflixPaymentScreenState extends State<NetflixPaymentScreen> {
+  int progressIndicator = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: context.background,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Stack(
+            children: [
+              if (progressIndicator == 0) ...{
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: _PaymentFeilds(
+                    mediaQuery: widget.mediaQuery,
+                    onTapped: () {
+                      setState(() {
+                        progressIndicator = 1;
+                      });
+                    },
+                  ),
+                )
+              },
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: CustomAppbar(
+                  title: 'Netflix Payment',
+                  leftIcon: 'icon_arrow_left',
+                  rightIcon: 'icon_notification',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PaymentFeilds extends StatefulWidget {
+  const _PaymentFeilds({
+    required this.mediaQuery,
+    required this.onTapped,
+  });
+  final MediaQueryHandler mediaQuery;
+  final VoidCallback onTapped;
+
+  @override
+  State<_PaymentFeilds> createState() => _PaymentFeildsState();
+}
+
+class _PaymentFeildsState extends State<_PaymentFeilds> {
+  late TextEditingController firstNameController;
+  late TextEditingController lastNameController;
+  late TextEditingController addressController;
+  late TextEditingController zipCodeController;
+  late TextEditingController stateController;
+  late TextEditingController cityController;
+  late TextEditingController countryController;
+  late TextEditingController cardHolderNameController;
+  late TextEditingController cardNumberController;
+  late TextEditingController dateController;
+  late TextEditingController cvvController;
+
+  @override
+  void initState() {
+    super.initState();
+    firstNameController = TextEditingController();
+    lastNameController = TextEditingController();
+    addressController = TextEditingController();
+    zipCodeController = TextEditingController();
+    stateController = TextEditingController();
+    cityController = TextEditingController();
+    countryController = TextEditingController();
+    cardHolderNameController = TextEditingController();
+    cardNumberController = TextEditingController();
+    dateController = TextEditingController();
+    cvvController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    firstNameController.dispose();
+    lastNameController.dispose();
+    addressController.dispose();
+    zipCodeController.dispose();
+    stateController.dispose();
+    cityController.dispose();
+    countryController.dispose();
+    cardHolderNameController.dispose();
+    cardNumberController.dispose();
+    dateController.dispose();
+    cvvController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomBox(
+            height: 81,
+            mediaQuery: widget.mediaQuery,
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: 51,
+                    width: 51,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        'icon_netflix'.toSvg,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 15),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Netflix',
+                      style: TextStyle(
+                        fontFamily: 'SM',
+                        fontSize: 16,
+                        color: AppColor.blackColor,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Next Payment: ',
+                          style: TextStyle(
+                            fontFamily: 'SSB',
+                            fontSize: 14,
+                            color: AppColor.greyColor200,
+                          ),
+                        ),
+                        Text(
+                          '12/04',
+                          style: TextStyle(
+                            fontFamily: 'SSB',
+                            fontSize: 14,
+                            color: AppColor.blueColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: '\$',
+                        style: TextStyle(
+                          fontFamily: 'SR',
+                          fontSize: 20,
+                          color: AppColor.blackColor,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '1.00',
+                        style: TextStyle(
+                          fontFamily: 'SB',
+                          fontSize: 20,
+                          color: AppColor.blackColor,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'USD',
+                        style: TextStyle(
+                          fontFamily: 'SM',
+                          fontSize: 10,
+                          color: AppColor.blackColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 35),
+          Text(
+            'Add Information',
+            style: context.headlineMedium,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'First Name',
+              controller: firstNameController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Last Name',
+              controller: lastNameController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Address',
+              controller: addressController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: 60,
+                  child: CustomTextField(
+                    mediaQuery: widget.mediaQuery,
+                    hint: 'Postal Code',
+                    controller: zipCodeController,
+                    color: context.secondaryContainer,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: SizedBox(
+                  child: CustomTextField(
+                    mediaQuery: widget.mediaQuery,
+                    hint: 'State',
+                    controller: stateController,
+                    color: context.secondaryContainer,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: 60,
+                  child: CustomTextField(
+                    mediaQuery: widget.mediaQuery,
+                    hint: 'City',
+                    controller: cityController,
+                    color: context.secondaryContainer,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: SizedBox(
+                  child: CustomTextField(
+                    mediaQuery: widget.mediaQuery,
+                    hint: 'Country',
+                    controller: countryController,
+                    color: context.secondaryContainer,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          Text(
+            'Add Acount Details',
+            style: context.headlineMedium,
+          ),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Card Holder Name',
+              controller: cardHolderNameController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Card Number',
+              controller: cardNumberController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: 60,
+                  child: CustomTextField(
+                    mediaQuery: widget.mediaQuery,
+                    hint: 'MM/YY',
+                    controller: dateController,
+                    color: context.secondaryContainer,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: SizedBox(
+                  child: CustomTextField(
+                    mediaQuery: widget.mediaQuery,
+                    hint: 'CVV',
+                    controller: cvvController,
+                    color: context.secondaryContainer,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 50),
+          SizedBox(
+            height: 63,
+            width: widget.mediaQuery.screenWidth(context),
+            child: ElevatedButton(
+              onPressed: () {
+                widget.onTapped();
+              },
+              child: const Text(
+                'Send',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+}
