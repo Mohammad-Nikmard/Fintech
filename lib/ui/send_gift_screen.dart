847f221bdbd33460843dcfdd25026964bd9148d9
MohammadNikmard <mnikmard2004@gmail.com>
2024-07-29 09:47:57 +0330

    feat: add gift fields

--- lib/ui/send_gift_screen.dart
+++ lib/ui/send_gift_screen.dart
@@ -1,8 +1,12 @@
 import 'package:fintech/constatns/color_constants.dart';
+import 'package:fintech/util/extensions/string_extension.dart';
 import 'package:fintech/util/extensions/theme_extension.dart';
 import 'package:fintech/util/mediaquery_handler.dart';
+import 'package:fintech/widget/custom_amount_selection_box.dart';
 import 'package:fintech/widget/custom_appbar.dart';
+import 'package:fintech/widget/my_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SendGiftScreen extends StatefulWidget {
   const SendGiftScreen({
@@ -42,6 +46,24 @@ class _SendGiftScreenState extends State<SendGiftScreen> {
                     selectedGift: (value) {
                       setState(() {
                         gift = value;
+                      });
+                    },
+                  ),
+                ),
+              } else if (progressIndicator == 1) ...{
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: _GiftFields(
+                    gift: gift,
+                    mediaQuery: widget.mediaQuery,
+                    onTapped: () {
+                      setState(() {
+                        progressIndicator = 2;
+                      });
+                    },
+                    selectedAmount: (value) {
+                      setState(() {
+                        amount = value;
                       });
                     },
                   ),
@@ -225,6 +247,329 @@ class _GiftListState extends State<_GiftList> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  const _AppBar({
+    required this.title,
+    required this.mediaQuery,
+  });
+  final String title;
+  final MediaQueryHandler mediaQuery;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35),
+      child: Container(
+        width: mediaQuery.screenWidth(context),
+        height: 239,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 0),
+              spreadRadius: -18,
+              blurRadius: 25,
+              color: AppColor.greyColor200,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              child: SizedBox(
+                height: 155,
+                width: double.infinity,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Image.asset(
+                    'assets/images/$title.png',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontFamily: 'SSB',
+                      fontSize: 20,
+                      color: AppColor.blackColor,
+                    ),
+                  ),
+                  Text(
+                    'Sned $title to your loved ones',
+                    style: const TextStyle(
+                      fontFamily: 'SR',
+                      fontSize: 15,
+                      color: AppColor.greyColor200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _GiftFields extends StatefulWidget {
+  const _GiftFields({
+    required this.gift,
+    required this.mediaQuery,
+    required this.onTapped,
+    required this.selectedAmount,
+  });
+  final MediaQueryHandler mediaQuery;
+  final String gift;
+  final VoidCallback onTapped;
+  final ValueChanged<String> selectedAmount;
+
+  @override
+  State<_GiftFields> createState() => _GiftFieldsState();
+}
+
+class _GiftFieldsState extends State<_GiftFields> {
+  late TextEditingController nameController;
+  late TextEditingController amountController;
+  late TextEditingController mobileNumberController;
+  late TextEditingController purposeOfPaymentController;
+  late TextEditingController passwordController;
+  late TextEditingController giftMessageController;
+
+  bool visibleEye = false;
+  String selectedAmount = '';
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController();
+    amountController = TextEditingController();
+    mobileNumberController = TextEditingController();
+    purposeOfPaymentController = TextEditingController();
+    passwordController = TextEditingController();
+    giftMessageController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    amountController.dispose();
+    mobileNumberController.dispose();
+    purposeOfPaymentController.dispose();
+    passwordController.dispose();
+    giftMessageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _AppBar(
+            title: widget.gift,
+            mediaQuery: widget.mediaQuery,
+          ),
+          Text(
+            'Add Recipient Bank Detail',
+            style: context.headlineMedium,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Name',
+              controller: nameController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Enter Account Number',
+              controller: amountController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: "Reciever's Mobile Number",
+              controller: mobileNumberController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          SizedBox(
+            height: 60,
+            width: widget.mediaQuery.screenWidth(context),
+            child: CustomTextField(
+              mediaQuery: widget.mediaQuery,
+              hint: 'Purpose of payment (Optional)',
+              controller: purposeOfPaymentController,
+              color: context.secondaryContainer,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  height: 60,
+                  child: TextField(
+                    controller: passwordController,
+                    style: const TextStyle(
+                      fontFamily: 'SM',
+                      fontSize: 16,
+                      color: AppColor.blackColor,
+                    ),
+                    decoration: InputDecoration(
+                      suffixIcon: visibleEye
+                          ? GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  visibleEye = !visibleEye;
+                                });
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(14.0),
+                                child: SvgPicture.asset(
+                                  'icon_eye_off'.toSvg,
+                                  colorFilter: const ColorFilter.mode(
+                                    AppColor.greyColor200,
+                                    BlendMode.srcIn,
+                                  ),
+                                ),
+                              ),
+                            )
+                          : GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  visibleEye = !visibleEye;
+                                });
+                              },
+                              child: const Icon(
+                                Icons.remove_red_eye,
+                                color: AppColor.greyColor200,
+                                size: 18.7,
+                              ),
+                            ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 23, vertical: 22),
+                      filled: true,
+                      fillColor: context.secondaryContainer,
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(
+                        fontFamily: 'SM',
+                        fontSize: 16,
+                        color: AppColor.greyColor200,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 17.25),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                child: ColoredBox(
+                  color: context.secondaryContainer,
+                  child: SizedBox(
+                    height: 63,
+                    width: 66,
+                    child: Padding(
+                      padding: const EdgeInsets.all(11.0),
+                      child: SvgPicture.asset(
+                        'icon_fingerprint'.toSvg,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 30),
+          CustomAmountSelectionBox(
+            mediaQuery: widget.mediaQuery,
+            selectedAmount: (value) {
+              setState(() {
+                selectedAmount = value;
+              });
+            },
+          ),
+          const SizedBox(height: 30),
+          Text(
+            'Enter Gift Messge',
+            style: context.headlineMedium,
+          ),
+          const SizedBox(height: 15),
+          CustomTextField(
+            maxLines: 4,
+            mediaQuery: widget.mediaQuery,
+            hint: 'Type your message here...',
+            controller: purposeOfPaymentController,
+            color: context.secondaryContainer,
+          ),
+          const SizedBox(height: 30),
+          SizedBox(
+            height: 63,
+            width: widget.mediaQuery.screenWidth(context),
+            child: ElevatedButton(
+              onPressed: () {
+                widget.selectedAmount(selectedAmount);
+                widget.onTapped();
+              },
+              child: const Text(
+                'Send',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+        ],
+      ),
     );
   }
 }
