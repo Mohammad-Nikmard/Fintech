84e8084f2ef039311a179c021ca55ef749ecc598
MohammadNikmard <mnikmard2004@gmail.com>
2024-07-30 20:30:20 +0330

    feat: add donation detail

--- lib/ui/donation_screen.dart
+++ lib/ui/donation_screen.dart
@@ -1,8 +1,12 @@
 import 'package:fintech/constatns/color_constants.dart';
+import 'package:fintech/util/extensions/string_extension.dart';
 import 'package:fintech/util/extensions/theme_extension.dart';
 import 'package:fintech/util/mediaquery_handler.dart';
+import 'package:fintech/widget/custom_amount_input.dart';
 import 'package:fintech/widget/custom_appbar.dart';
+import 'package:fintech/widget/custom_box.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DonationScreen extends StatefulWidget {
   const DonationScreen({
@@ -19,6 +23,7 @@ class _DonationScreenState extends State<DonationScreen> {
   int progressIndicator = 0;
   String donation = '';
   String amount = '';
+  String donationOwner = '';
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +38,11 @@ class _DonationScreenState extends State<DonationScreen> {
                 Padding(
                   padding: const EdgeInsets.only(top: 80),
                   child: _CharityList(
+                    dontationOwner: (value) {
+                      setState(() {
+                        donationOwner = value;
+                      });
+                    },
                     mediaQuery: widget.mediaQuery,
                     onTapped: () {
                       setState(() {
@@ -44,6 +54,25 @@ class _DonationScreenState extends State<DonationScreen> {
                         donation = value;
                       });
                     },
+                  ),
+                ),
+              } else if (progressIndicator == 1) ...{
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: _DonationDetails(
+                    onTapped: () {
+                      setState(() {
+                        progressIndicator = 2;
+                      });
+                    },
+                    enteredAmount: (value) {
+                      setState(() {
+                        amount = value;
+                      });
+                    },
+                    mediaQuery: widget.mediaQuery,
+                    selectedDonation: donation,
+                    selectedOwner: donationOwner,
                   ),
                 ),
               },
@@ -87,10 +116,12 @@ class _CharityList extends StatefulWidget {
     required this.mediaQuery,
     required this.onTapped,
     required this.selectedCharity,
+    required this.dontationOwner,
   });
   final MediaQueryHandler mediaQuery;
   final VoidCallback onTapped;
   final ValueChanged<String> selectedCharity;
+  final ValueChanged<String> dontationOwner;
 
   @override
   State<_CharityList> createState() => _CharityListState();
@@ -309,6 +340,7 @@ class _CharityListState extends State<_CharityList> {
                                       Radius.circular(10)),
                                   child: GestureDetector(
                                     onTap: () {
+                                      widget.dontationOwner(ownerList[index]);
                                       widget
                                           .selectedCharity(charityList[index]);
                                       widget.onTapped();
@@ -345,6 +377,318 @@ class _CharityListState extends State<_CharityList> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  const _AppBar({
+    required this.title,
+    required this.owner,
+    required this.mediaQuery,
+  });
+  final String title;
+  final String owner;
+  final MediaQueryHandler mediaQuery;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35),
+      child: Container(
+        width: mediaQuery.screenWidth(context),
+        height: 260,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 0),
+              spreadRadius: -18,
+              blurRadius: 25,
+              color: AppColor.greyColor200,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              child: SizedBox(
+                height: 155,
+                width: double.infinity,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Image.asset(
+                    'assets/images/${title.substring(11)}.png',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  Text(
+                    'Donate for $title',
+                    style: const TextStyle(
+                      fontFamily: 'SSB',
+                      fontSize: 18,
+                      color: AppColor.blackColor,
+                    ),
+                  ),
+                  Text(
+                    'Arrange by $owner',
+                    style: const TextStyle(
+                      fontFamily: 'SR',
+                      fontSize: 15,
+                      color: AppColor.greyColor200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DonationDetails extends StatelessWidget {
+  const _DonationDetails({
+    required this.mediaQuery,
+    required this.selectedDonation,
+    required this.selectedOwner,
+    required this.enteredAmount,
+    required this.onTapped,
+  });
+  final MediaQueryHandler mediaQuery;
+  final String selectedOwner;
+  final String selectedDonation;
+  final ValueChanged<String> enteredAmount;
+  final VoidCallback onTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _AppBar(
+          title: selectedDonation,
+          owner: selectedOwner,
+          mediaQuery: mediaQuery,
+        ),
+        CustomBox(
+          height: 91,
+          mediaQuery: mediaQuery,
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                child: SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: ColoredBox(
+                    color: AppColor.greenLight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SvgPicture.asset(
+                        'icon_checkmark'.toSvg,
+                        colorFilter: const ColorFilter.mode(
+                          AppColor.greenDark,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        'Heads Foundation ',
+                        style: TextStyle(
+                          fontFamily: 'SSB',
+                          fontSize: 20,
+                          color: AppColor.blackColor,
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        'icon_approval'.toSvg,
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    'Verified Foundation',
+                    style: TextStyle(
+                      fontFamily: 'SR',
+                      fontSize: 13,
+                      color: AppColor.blackColor,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 35),
+        Text(
+          'Participants',
+          style: context.headlineMedium,
+        ),
+        const SizedBox(height: 15),
+        Row(
+          children: [
+            Stack(
+              children: [
+                const _CircularProfile(
+                  picture: DecorationImage(
+                    image: AssetImage('assets/images/my_photo.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: _CircularProfile(
+                    backgroundColor: Colors.yellow,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 60),
+                  child: _CircularProfile(
+                    backgroundColor: Colors.teal,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 90),
+                  child: _CircularProfile(
+                    backgroundColor: Colors.redAccent,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 120),
+                  child: _CircularProfile(
+                    backgroundColor: context.secondaryContainer,
+                    child: const Center(
+                      child: Text(
+                        '9+',
+                        style: TextStyle(
+                          fontFamily: 'SR',
+                          color: AppColor.greyColor200,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(7),
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: const SizedBox(
+                  height: 32,
+                  width: 102,
+                  child: ColoredBox(
+                    color: AppColor.blueColor,
+                    child: Center(
+                      child: Text(
+                        'See all',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 30),
+          child: SizedBox(
+            height: 63,
+            width: mediaQuery.screenWidth(context),
+            child: ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (context) {
+                    return CustomAmountInputSheet(
+                      onTapped: () {
+                        onTapped();
+                      },
+                      mediaQuery: mediaQuery,
+                      eneterdAmount: (value) {
+                        enteredAmount(value);
+                      },
+                    );
+                  },
+                  backgroundColor: Colors.transparent,
+                );
+              },
+              child: const Text(
+                'Donate Now',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CircularProfile extends StatelessWidget {
+  const _CircularProfile({
+    this.backgroundColor,
+    this.picture,
+    this.child,
+  });
+  final Color? backgroundColor;
+  final DecorationImage? picture;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      width: 48,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: backgroundColor,
+        image: picture,
+        border: Border.all(
+          width: 1,
+          color: Colors.white,
+        ),
+      ),
+      child: child,
     );
   }
 }
---
+++ lib/widget/custom_amount_input.dart
@@ -0,0 +1,320 @@
+import 'package:fintech/constatns/color_constants.dart';
+import 'package:fintech/util/extensions/string_extension.dart';
+import 'package:fintech/util/extensions/theme_extension.dart';
+import 'package:fintech/util/mediaquery_handler.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CustomAmountInputSheet extends StatefulWidget {
+  const CustomAmountInputSheet({
+    super.key,
+    required this.mediaQuery,
+    required this.eneterdAmount,
+    required this.onTapped,
+  });
+  final MediaQueryHandler mediaQuery;
+  final ValueChanged<String> eneterdAmount;
+  final VoidCallback onTapped;
+
+  @override
+  State<CustomAmountInputSheet> createState() => _CustomAmountInputSheetState();
+}
+
+class _CustomAmountInputSheetState extends State<CustomAmountInputSheet> {
+  late TextEditingController amountController;
+  int quickActionIndex = 0;
+
+  List<String> priceList = [
+    '\$50',
+    '\$100',
+    '\$150',
+    '\$200',
+    '\$250',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    amountController = TextEditingController(text: '\$');
+  }
+
+  @override
+  void dispose() {
+    amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 770,
+      width: widget.mediaQuery.screenWidth(context),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20, right: 25, left: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    'Enter Amount',
+                    style: context.headlineMedium,
+                  ),
+                ),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: SvgPicture.asset(
+                    'icon_close'.toSvg,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              height: 65,
+              width: widget.mediaQuery.screenWidth(context),
+              child: TextField(
+                style: const TextStyle(
+                  fontFamily: 'SB',
+                  fontSize: 28,
+                  color: AppColor.blueColor,
+                ),
+                controller: amountController,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.only(bottom: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                    borderSide: BorderSide(
+                      color: AppColor.blueColor,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7),
+                    ),
+                    borderSide: BorderSide(
+                      color: AppColor.blueColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Quick Actions',
+              style: context.headlineMedium,
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              height: 48,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          quickActionIndex = index;
+                          amountController.text = priceList[quickActionIndex];
+                        });
+                      },
+                      child: Container(
+                        height: 48,
+                        width: 114,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(7),
+                          ),
+                          color: (quickActionIndex == index)
+                              ? Colors.transparent
+                              : context.secondaryContainer,
+                          border: Border.all(
+                            color: (quickActionIndex == index)
+                                ? AppColor.blueColor
+                                : Colors.transparent,
+                            width: 1,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            priceList[index],
+                            style: const TextStyle(
+                              fontFamily: 'SM',
+                              fontSize: 22,
+                              color: AppColor.greyColor200,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: priceList.length,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              child: SizedBox(
+                height: 63,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    widget.eneterdAmount(amountController.text);
+                    Navigator.pop(context);
+                    widget.onTapped();
+                  },
+                  child: const Text(
+                    'Next',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 35),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      button('1'),
+                      button('2'),
+                      button('3'),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      button('4'),
+                      button('5'),
+                      button('6'),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      button('7'),
+                      button('8'),
+                      button('9'),
+                    ],
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            shape: const CircleBorder(),
+                            backgroundColor: AppColor.blueColor,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              amountController.text = '\$';
+                            });
+                          },
+                          child: SvgPicture.asset(
+                            'icon_close'.toSvg,
+                            height: 14,
+                            width: 14,
+                            colorFilter: const ColorFilter.mode(
+                              Colors.white,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                        ),
+                      ),
+                      button('0'),
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            shape: const CircleBorder(),
+                            backgroundColor: AppColor.blueColor,
+                          ),
+                          onPressed: () {
+                            if (amountController.text.length > 1) {
+                              setState(() {
+                                amountController.text = amountController.text
+                                    .substring(
+                                        0, amountController.text.length - 1);
+                              });
+                            }
+                          },
+                          child: SvgPicture.asset(
+                            'icon_arrow_right'.toSvg,
+                            height: 21,
+                            width: 21,
+                            colorFilter: const ColorFilter.mode(
+                              Colors.white,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget button(String text) {
+    return SizedBox(
+      height: 70,
+      width: 70,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          shape: const CircleBorder(),
+          backgroundColor: context.secondaryContainer,
+        ),
+        onPressed: () {
+          setState(() {
+            amountController.text += text;
+          });
+        },
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontFamily: 'SR',
+            color: AppColor.greyColor200,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
